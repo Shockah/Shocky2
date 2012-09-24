7d2f65c36f6d7704855902d306a68893d1b262ff
Shockah <lolshockah@gmail.com>
2012-09-24 10:14:51 +0200

    Changing classpath, adding ANT build script

--- modulesrc/pl/shockah/shocky2/module/botcontrol/Module.java
+++ modulesrc/pl/shockah/shocky2/module/botcontrol/Module.java
@@ -1,7 +1,10 @@
 package pl.shockah.shocky2.module.botcontrol;
 
-public final class Module extends pl.shockah.shocky2.Module {
+public final class Module extends pl.shockah.shocky2.ModuleWithCommands {
 	public String name() {return "botcontrol";}
 	public String info() {return "Allows basic bot control";}
-	protected boolean canDisable() {return false;}
+	
+	public void onEnable() {
+		addCommand(new CommandJoin());
+	}
 }
